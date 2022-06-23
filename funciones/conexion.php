e90ac61cd8e3eee614ceb4dd07bc38348999ca23
Rodrigo <108javierr@gmail.com>
2022-06-23 18:10:01 -0500

    Subiendo el proyecto

---
+++ funciones/conexion.php
@@ -0,0 +1,13 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $password = "14040002";
+    $db = "bd_zapateria";
+
+    $conexion = mysqli_connect(
+        $servidor,
+        $usuario,
+        $password,
+        $db) OR DIE
+        ("No se puede conectar......".mysqli_connect_error());
+?>
