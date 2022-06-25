79c45d524881c146bb5274d116a6bf7afcfd1aa9
juanpablo189 <juanpated2317@gmail.com>
2022-06-25 12:38:41 -0500

    index modificacion

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
         <section class="page-section bg-primary text-white mb-0" id="about">
             <div class="container">
                 <!-- About Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <h2 class="page-section-heading text-center text-uppercase text-white">Conocenos</h2>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -138,16 +138,10 @@
                 </div>
                 <!-- About Section Content-->
                 <div class="row">
-                    <div class="col-lg-4 ms-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-                    <div class="col-lg-4 me-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+                    <div class="col-lg-4 ms-auto"><p class="lead">Somos una Zapateria muy confiable, contamos con todo tipo de calzado con la finalidad de poder ofrder a nuestros clientes el mejor servicio y poder brindar una mejor atención.</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">Nuestros Zapatos son de la mejor calidad, poseen los materiales más recistentes y duraderos que hay en el mercado, para poder ofrecer calidad y confort a nuestros clientes.</p></div>
                 </div>
                 <!-- About Section Button-->
-                <div class="text-center mt-4">
-                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
-                        <i class="fas fa-download me-2"></i>
-                        Free Download!
-                    </a>
-                </div>
             </div>
         </section>  
         <!-- Footer-->
