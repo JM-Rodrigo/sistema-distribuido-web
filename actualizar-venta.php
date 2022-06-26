fd78362013a7d80f059d792a99427f6934334306
juanpablo189 <juanpated2317@gmail.com>
2022-06-26 13:24:15 -0500

    actualizar

--- actualizar-venta.php
+++ actualizar-venta.php
@@ -89,22 +89,22 @@
 
                                 <form action="funciones/update-venta.php" method="POST" id="contactForm" data-sb-form-api-token="API_TOKEN">
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" name="cmbCodigo"  type="text" minlength="1"  maxlength="6" placeholder="Rellena el campo" data-sb-validations="required" value="<?php echo $producto['Codigo'];?>"/>
+                                        <input class="form-control" name="cmbCodigo"  type="text" minlength="1"  maxlength="6" placeholder="Rellena el campo" data-sb-validations="required" value="<?php echo $venta['Codigo'];?>"/>
                                         <label for="name" style="color: rgb(0, 0, 0);">Código</label>
                                         <div class="invalid-feedback" data-sb-feedback="name:required">Es necesario llenar esté campo</div>
                                     </div>
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" name="txtFechaVenta" type="text"  minlength="1"  maxlength="50" placeholder="Rellena el campo" style="height: 7rem" data-sb-validations="required" value="<?php echo $producto['FechaVenta'];?>"/>
+                                        <input class="form-control" name="txtFechaVenta" type="date"  minlength="1"  maxlength="50" placeholder="Rellena el campo" style="height: 7rem" data-sb-validations="required" value="<?php echo $venta['FechaVenta'];?>"/>
                                         <label for="message" style="color: rgb(0, 0, 0);">Fecha</label>
                                         <div class="invalid-feedback" data-sb-feedback="message:required">Es necesario llenar esté campo</div>
                                     </div>
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" name="txtCantidad"  type="text"  minlength="1"  maxlength="30" placeholder="Rellena el campo" data-sb-validations="required" value="<?php echo $producto['Cantidad'];?>"/>
+                                        <input class="form-control" name="txtCantidad"  type="number"  minlength="1"  maxlength="30" placeholder="Rellena el campo" data-sb-validations="required" value="<?php echo $venta['Cantidad'];?>"/>
                                         <label for="name" style="color: rgb(0, 0, 0);">Cantidad</label>
                                         <div class="invalid-feedback" data-sb-feedback="name:required">Es necesario llenar esté campo</div>
                                     </div>
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" name="txtPrecio" type="number" step="0.01" minlength="1"  maxlength="10" placeholder="Rellena el campo" data-sb-validations="required" value="<?php echo $producto['PrecioVenta'];?>" />
+                                        <input class="form-control" name="txtPrecio" type="number" step="" minlength="1"  maxlength="10" placeholder="Rellena el campo" data-sb-validations="required" value="<?php echo $venta['PrecioVenta'];?>" />
                                         <label for="name" style="color: rgb(0, 0, 0);">Precio</label>
                                         <div class="invalid-feedback" data-sb-feedback="name:required">Es necesario llenar esté campo</div>
                                     </div>
