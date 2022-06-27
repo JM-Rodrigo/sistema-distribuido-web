b30a29ffa0e7a64fbb5c7e332d1ee1dd0ee9dafc
Jonathan070918 <brandonmedinamart@gmail.com>
2022-06-27 14:56:24 -0500

    modifi2

---
+++ funciones/create-compra.php
@@ -0,0 +1,25 @@
+<?php
+    include("conexionJonathan.php");
+
+    $idCompra= $_POST['cmbCodigo'];
+    $fechaCompra= $_POST['txtFechaVenta'];
+    $catidadCompra= $_POST['txtCantidad'];
+    $precioCompra= $_POST['txtPrecio'];
+    
+    $sentencia = "INSERT INTO compracalzado VALUES(
+        DEFAULT,
+        '$idCompra',
+        '$fechaCompra',
+        $catidadCompra,
+        '$precioCompra'
+        )";
+
+    if(mysqli_query($conexion,$sentencia)){
+        header("Location: ../compra.php");
+    }else{
+        echo "error".mysqli_connect_error();
+    }
+    
+    mysqli_close($conexion);
+
+?>
