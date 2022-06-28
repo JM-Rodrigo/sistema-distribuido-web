40f9517d7163ad110a54644d45017df616bf3a83
Jonathan070918 <brandonmedinamart@gmail.com>
2022-06-28 16:24:18 -0500

    hola

--- funciones/conexionJonathan.php
+++ funciones/conexionJonathan.php
@@ -2,7 +2,7 @@
     $servidor = "localhost";
     $usuario = "root";
     $password = "jonathan070918";
-    $db = "zapateria1";
+    $db = "zapateria";
 
     $conexion = mysqli_connect(
         $servidor,
