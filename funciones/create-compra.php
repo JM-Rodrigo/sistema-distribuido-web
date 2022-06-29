d7b2b2ba581fd7585dcd779ac9e4f42320e26610
Jonathan070918 <brandonmedinamart@gmail.com>
2022-06-29 13:58:28 -0500

    modificacion

--- funciones/create-compra.php
+++ funciones/create-compra.php
@@ -7,21 +7,29 @@
     $FechaHoraCompra= $_POST['txtFechaCompra'];
     $PrecioCompra= $_POST['txtPrecioCompra'];
     $proveedor = $_POST['txtProveedor'];
-    
-    $sentencia = "INSERT INTO compra VALUES(
-        DEFAULT,
-        '$Codigo',
-        '$CantidadCompra',
-        '$FechaHoraCompra',
-        '$PrecioCompra',
-        '$proveedor'                                                   
-        )";
 
-    if(mysqli_query($conexion,$sentencia)){
-        header("Location: ../compra.php");
+    $sentencia2 = "UPDATE calzado SET Stock = Stock + $CantidadCompra WHERE Codigo = '$Codigo'";
+    
+    if(mysqli_query($conexion,$sentencia2)){
+        $sentencia = "INSERT INTO compra VALUES(
+            DEFAULT,
+            '$Codigo',
+            '$CantidadCompra',
+            '$FechaHoraCompra',
+            '$PrecioCompra',
+            '$proveedor'                                                   
+            )";
+    
+        if(mysqli_query($conexion,$sentencia)){
+            header("Location: ../compra.php");
+        }else{
+            echo "error".mysqli_connect_error();
+        }
     }else{
         echo "error".mysqli_connect_error();
     }
+
+    
     
     mysqli_close($conexion);
 
